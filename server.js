b16fec74be553b458d91868f4f2b43b495976a50
Tuyen Chung <tuyen_chung@yahoo.com>
2015-09-09 15:43:53 -0700

    added views to check if app is running

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
-// app.use(express.static(path.join(__dirname, "./static")));
-// app.set('views', path.join(__dirname, "./views"));
-// app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "./static")));
+app.set('views', path.join(__dirname, "./views"));
+app.set('view engine', 'ejs');
 
-// app.get('/', function(req, res){
-// 	res.render('index');
-// })
+app.get('/', function(req, res){
+	res.render('index');
+})
 
 // var server = app.listen('https://instacash-node.herokuapp.com/');
 
 
-var server = app.listen(8000);
+var server = app.listen(5000);
 var io = require('socket.io').listen(server);
 
 var current_user_id = {};
